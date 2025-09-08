fa1fd2dd970eede44cd4b88d4367a805c761b6fa
grave0718 <grave0718@gmail.com>
2025-09-08 10:41:08 +0900

    캠로테이트코드 완성

--- Assets/Scripts/CamRotate.cs
+++ Assets/Scripts/CamRotate.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CamRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 angle;
+    public float sensitivity = 200;
+
     void Start()
     {
-        
+        angle = Camera.main.transform.eulerAngles;
+        angle.x *= -1;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //마우스 입력
+        float x = Input.GetAxis("Mouse Y");
+        float y = Input.GetAxis("Mouse X");
+        //이동방향 별 회전 값 누적
+        angle.x += x * sensitivity * Time.deltaTime;
+        angle.y += y * sensitivity * Time.deltaTime;
         
+        angle.x = Mathf.Clamp(angle.x,-90,90);
+        //실제 회전
+        transform.eulerAngles = new Vector3(-angle.x, angle.y, transform.eulerAngles.z);
+
     }
 }
