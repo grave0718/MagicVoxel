b7e132c35c3fa2e6a46b78dde4955a80785fa8a3
grave0718 <grave0718@gmail.com>
2025-09-15 10:56:25 +0900

    마우스따라생성 + 색랜덤

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class Voxel : MonoBehaviour
 {
     public float speed = 5;
-    float currentTime=0;
+    float currentTime = 0;
     public float destroyTime = 3f;
-    void Start()
-    {
-        Vector3 direction = Random.insideUnitSphere;
 
-        Rigidbody rb= gameObject.GetComponent<Rigidbody>();
+    void OnEnable()
+    {
+
+        currentTime = 0;
+        Vector3 direction = Random.insideUnitSphere;
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.velocity = direction * speed;
-       
+        
     }
-    void Update(){
+    void Update()
+    {
         currentTime += Time.deltaTime;
 
-        if(currentTime > destroyTime){
-            Destroy(gameObject);
+        if (currentTime > destroyTime)
+        {
+            gameObject.SetActive(false);
+            VoxelMaker.voxelPool.Add(gameObject);
+            
         }
     }
 
- 
+
 }
--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -6,22 +6,48 @@ public class VoxelMaker : MonoBehaviour
 {
     public GameObject voxelFactory;
     public int voxelPoolSize = 20;
+    public float CreateTime = 0.1f;
+    float currentTime = 0;
+    //<Gameobject>는 제네럴, 리스트 요소의 기본 형태를 정해둠
+    //static은 이 코드가 여러번 실행되어 독립되더라도 이 변수는 유일하게 정의
+    public static List<GameObject> voxelPool = new List<GameObject>();
 
 
+    void Start()
+    {
+        for (int i = 0; i < voxelPoolSize; i++)
+        {
+            GameObject voxel = Instantiate(voxelFactory);
+            voxel.SetActive(false);
+            
+            voxelPool.Add(voxel);
 
 
+        }
+
+    }
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")||Input.GetButtonDown("Fire2"))
+        currentTime += Time.deltaTime;
+
+        if (currentTime > CreateTime)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo = new RaycastHit();
 
             if (Physics.Raycast(ray, out hitInfo))
             {
-                GameObject voxel = Instantiate(voxelFactory);
-                voxel.transform.position = hitInfo.point;
+                if (voxelPool.Count > 0)
+                {
+                    currentTime = 0f;
+                    GameObject voxel = voxelPool[0]; //복셀풀에서 첫번째값을 가져옴
+                    voxel.SetActive(true);
+                    voxel.transform.position = hitInfo.point; //원하는 위치에 생성
+                    voxelPool.RemoveAt(0); //복셀풀에서 첫번째값 제거   
+                }
+
             }
+
         }
     }
 }
