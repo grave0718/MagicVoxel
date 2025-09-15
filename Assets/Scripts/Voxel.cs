f50850555bedcba39e2cc0607096a2601904e280
grave0718 <grave0718@gmail.com>
2025-09-15 11:09:07 +0900

    주석추가

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -13,7 +13,7 @@ public class Voxel : MonoBehaviour
 
         currentTime = 0;
         Vector3 direction = Random.insideUnitSphere;
-        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f); //system, unityengine 라이브러리 둘다 쓰면 둘중 어디에서 무슨 랜덤을 가져와야할지 모르기 때문에 오류 발생, 어떤 랜덤을 사용할지 지정해줘야함
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.velocity = direction * speed;
         
